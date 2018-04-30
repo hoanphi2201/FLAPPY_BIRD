a9c32a69152f51f1c5d8e3a7e1696adfcf5f5321
hoanphi2201 <phihoan10@gmail.com>
2018-04-30 14:10:49 +0700

    Ver 1

---
+++ Game Flappy Bird/Ver 1/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Ver_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Background_Pic = new System.Windows.Forms.PictureBox();
+            this.OngCongTren1 = new System.Windows.Forms.PictureBox();
+            this.OngCongDuoi3 = new System.Windows.Forms.PictureBox();
+            this.OngCongDuoi2 = new System.Windows.Forms.PictureBox();
+            this.OngCongTren3 = new System.Windows.Forms.PictureBox();
+            this.OngCongTren2 = new System.Windows.Forms.PictureBox();
+            this.OngCongDuoi1 = new System.Windows.Forms.PictureBox();
+            this.ConChim = new System.Windows.Forms.PictureBox();
+            this.Timer_Chay = new System.Windows.Forms.Timer(this.components);
+            this.Play = new System.Windows.Forms.PictureBox();
+            this.TimerChim = new System.Windows.Forms.Timer(this.components);
+            this.TimerChim2 = new System.Windows.Forms.Timer(this.components);
+            this.TimerChim3 = new System.Windows.Forms.Timer(this.components);
+            this.lblDiemSo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Background_Pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OngCongTren1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OngCongDuoi3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OngCongDuoi2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OngCongTren3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OngCongTren2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OngCongDuoi1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ConChim)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Play)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Background_Pic
+            // 
+            this.Background_Pic.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Background_Pic.BackgroundImage")));
+            this.Background_Pic.Location = new System.Drawing.Point(0, 0);
+            this.Background_Pic.Name = "Background_Pic";
+            this.Background_Pic.Size = new System.Drawing.Size(1518, 749);
+            this.Background_Pic.TabIndex = 0;
+            this.Background_Pic.TabStop = false;
+            // 
+            // OngCongTren1
+            // 
+            this.OngCongTren1.BackColor = System.Drawing.Color.Teal;
+            this.OngCongTren1.Image = ((System.Drawing.Image)(resources.GetObject("OngCongTren1.Image")));
+            this.OngCongTren1.Location = new System.Drawing.Point(344, -416);
+            this.OngCongTren1.Name = "OngCongTren1";
+            this.OngCongTren1.Size = new System.Drawing.Size(184, 714);
+            this.OngCongTren1.TabIndex = 1;
+            this.OngCongTren1.TabStop = false;
+            // 
+            // OngCongDuoi3
+            // 
+            this.OngCongDuoi3.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.OngCongDuoi3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("OngCongDuoi3.BackgroundImage")));
+            this.OngCongDuoi3.Image = ((System.Drawing.Image)(resources.GetObject("OngCongDuoi3.Image")));
+            this.OngCongDuoi3.Location = new System.Drawing.Point(1020, 440);
+            this.OngCongDuoi3.Name = "OngCongDuoi3";
+            this.OngCongDuoi3.Size = new System.Drawing.Size(176, 733);
+            this.OngCongDuoi3.TabIndex = 10;
+            this.OngCongDuoi3.TabStop = false;
+            // 
+            // OngCongDuoi2
+            // 
+            this.OngCongDuoi2.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.OngCongDuoi2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("OngCongDuoi2.BackgroundImage")));
+            this.OngCongDuoi2.Image = ((System.Drawing.Image)(resources.GetObject("OngCongDuoi2.Image")));
+            this.OngCongDuoi2.Location = new System.Drawing.Point(676, 440);
+            this.OngCongDuoi2.Name = "OngCongDuoi2";
+            this.OngCongDuoi2.Size = new System.Drawing.Size(176, 733);
+            this.OngCongDuoi2.TabIndex = 9;
+            this.OngCongDuoi2.TabStop = false;
+            // 
+            // OngCongTren3
+            // 
+            this.OngCongTren3.BackColor = System.Drawing.Color.Teal;
+            this.OngCongTren3.Image = ((System.Drawing.Image)(resources.GetObject("OngCongTren3.Image")));
+            this.OngCongTren3.Location = new System.Drawing.Point(1012, -425);
+            this.OngCongTren3.Name = "OngCongTren3";
+            this.OngCongTren3.Size = new System.Drawing.Size(184, 714);
+            this.OngCongTren3.TabIndex = 8;
+            this.OngCongTren3.TabStop = false;
+            // 
+            // OngCongTren2
+            // 
+            this.OngCongTren2.BackColor = System.Drawing.Color.Teal;
+            this.OngCongTren2.Image = ((System.Drawing.Image)(resources.GetObject("OngCongTren2.Image")));
+            this.OngCongTren2.Location = new System.Drawing.Point(676, -425);
+            this.OngCongTren2.Name = "OngCongTren2";
+            this.OngCongTren2.Size = new System.Drawing.Size(184, 714);
+            this.OngCongTren2.TabIndex = 7;
+            this.OngCongTren2.TabStop = false;
+            // 
+            // OngCongDuoi1
+            // 
+            this.OngCongDuoi1.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.OngCongDuoi1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("OngCongDuoi1.BackgroundImage")));
+            this.OngCongDuoi1.Image = ((System.Drawing.Image)(resources.GetObject("OngCongDuoi1.Image")));
+            this.OngCongDuoi1.Location = new System.Drawing.Point(344, 440);
+            this.OngCongDuoi1.Name = "OngCongDuoi1";
+            this.OngCongDuoi1.Size = new System.Drawing.Size(176, 733);
+            this.OngCongDuoi1.TabIndex = 6;
+            this.OngCongDuoi1.TabStop = false;
+            // 
+            // ConChim
+            // 
+            this.ConChim.BackColor = System.Drawing.Color.Teal;
+            this.ConChim.Image = ((System.Drawing.Image)(resources.GetObject("ConChim.Image")));
+            this.ConChim.Location = new System.Drawing.Point(136, 309);
+            this.ConChim.Name = "ConChim";
+            this.ConChim.Size = new System.Drawing.Size(116, 81);
+            this.ConChim.TabIndex = 11;
+            this.ConChim.TabStop = false;
+            // 
+            // Timer_Chay
+            // 
+            this.Timer_Chay.Tick += new System.EventHandler(this.Timer_Chay_Tick);
+            // 
+            // Play
+            // 
+            this.Play.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Play.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Play.Image = ((System.Drawing.Image)(resources.GetObject("Play.Image")));
+            this.Play.Location = new System.Drawing.Point(0, 0);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(115, 49);
+            this.Play.TabIndex = 12;
+            this.Play.TabStop = false;
+            this.Play.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // TimerChim
+            // 
+            this.TimerChim.Tick += new System.EventHandler(this.TimerChim_Tick);
+            // 
+            // TimerChim2
+            // 
+            this.TimerChim2.Tick += new System.EventHandler(this.TimerChim2_Tick);
+            // 
+            // TimerChim3
+            // 
+            this.TimerChim3.Tick += new System.EventHandler(this.TimerChim3_Tick);
+            // 
+            // lblDiemSo
+            // 
+            this.lblDiemSo.AutoSize = true;
+            this.lblDiemSo.BackColor = System.Drawing.Color.DarkCyan;
+            this.lblDiemSo.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiemSo.Location = new System.Drawing.Point(150, 9);
+            this.lblDiemSo.Name = "lblDiemSo";
+            this.lblDiemSo.Size = new System.Drawing.Size(135, 45);
+            this.lblDiemSo.TabIndex = 13;
+            this.lblDiemSo.Text = "Điểm: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1541, 748);
+            this.Controls.Add(this.lblDiemSo);
+            this.Controls.Add(this.Play);
+            this.Controls.Add(this.ConChim);
+            this.Controls.Add(this.OngCongDuoi3);
+            this.Controls.Add(this.OngCongDuoi2);
+            this.Controls.Add(this.OngCongTren3);
+            this.Controls.Add(this.OngCongTren2);
+            this.Controls.Add(this.OngCongDuoi1);
+            this.Controls.Add(this.OngCongTren1);
+            this.Controls.Add(this.Background_Pic);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Flappy Bird";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Background_Pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OngCongTren1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OngCongDuoi3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OngCongDuoi2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OngCongTren3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OngCongTren2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OngCongDuoi1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ConChim)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Play)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Background_Pic;
+        private System.Windows.Forms.PictureBox OngCongTren1;
+        private System.Windows.Forms.PictureBox OngCongDuoi3;
+        private System.Windows.Forms.PictureBox OngCongDuoi2;
+        private System.Windows.Forms.PictureBox OngCongTren3;
+        private System.Windows.Forms.PictureBox OngCongTren2;
+        private System.Windows.Forms.PictureBox OngCongDuoi1;
+        private System.Windows.Forms.PictureBox ConChim;
+        private System.Windows.Forms.Timer Timer_Chay;
+        protected internal System.Windows.Forms.PictureBox Play;
+        private System.Windows.Forms.Timer TimerChim;
+        private System.Windows.Forms.Timer TimerChim2;
+        private System.Windows.Forms.Timer TimerChim3;
+        private System.Windows.Forms.Label lblDiemSo;
+    }
+}
+
---
+++ Game Flappy Bird/Ver 1/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+namespace Ver_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int x_cap1, x_cap2, x_cap3;
+        int x_ongtren1, y_ongtren1, x_ongtren2, y_ongtren2, x_ongtren3, y_ongtren3;
+        int DoChenhLechGiua2Ong = 200;
+        int x_ongduoi1, y_ongduoi1, x_ongduoi2, y_ongduoi2, x_ongduoi3, y_ongduoi3;
+        int x_ConChim, y_ConChim;
+        private void Refesh()
+        {
+            ConChim.Image = Image.FromFile(@"chim.png");
+            Background_Pic.Size = new Size(this.Width, this.Height);
+            // set background
+            OngCongDuoi1.Size = new Size(180, 730);
+            OngCongTren1.Size = new Size(180, 709);
+            OngCongDuoi2.Size = new Size(180, 730);
+            OngCongTren2.Size = new Size(180, 709);
+            OngCongDuoi3.Size = new Size(180, 730);
+            OngCongTren3.Size = new Size(180, 709);
+            // set llocation
+            // ống 1
+
+            // random độ cao
+            Random rd = new Random();
+            y_ongtren1 = -372; // độ cao ống trên  - 300 (tôi muốn ống đầu luôn có độ cao 300)
+            y_ongtren2 = rd.Next(-550, -250); // ống trên + y_oongstren = độ cao hiển thị
+            x_cap1 = this.Width + 300;
+            OngCongTren1.Location = new Point(x_cap1, y_ongtren1);
+            y_ongduoi1 = (DoChenhLechGiua2Ong + (OngCongTren1.Height + y_ongtren1));
+            OngCongDuoi1.Location = new Point(x_cap1, y_ongduoi1);
+            // ống 2
+            x_cap2 = this.Width + 300 + OngCongTren1.Width + 350;
+            y_ongtren2 = 400 - OngCongTren2.Height; // ống trên + y_oongstren = độ cao hiển thị
+            OngCongTren2.Location = new Point(x_cap2, y_ongtren2);
+            y_ongduoi2 = (DoChenhLechGiua2Ong + (OngCongTren2.Height + y_ongtren2));
+            OngCongDuoi2.Location = new Point(x_cap2, y_ongduoi2);
+            // ống 3
+            x_cap3 = this.Width + 300 + 2 * OngCongTren1.Width + 2 * 350;
+            Random rd3 = new Random();
+            y_ongtren3 = rd3.Next(-550, -250); // ống trên + y_oongstren = độ cao hiển thị
+            OngCongTren3.Location = new Point(x_cap3, y_ongtren3);
+            y_ongduoi3 = (DoChenhLechGiua2Ong + (OngCongTren3.Height + y_ongtren3));
+            OngCongDuoi3.Location = new Point(x_cap3, y_ongduoi3);
+
+            // Set con chim
+            ConChim.Size = new Size(122, 80);
+            ConChim.Parent = Background_Pic;
+            ConChim.BackColor = Color.Transparent;
+            x_ConChim = 136;
+            y_ConChim = 309;
+            //x_ConChim = ConChim.Location.X;
+            //y_ConChim = ConChim.Location.Y;
+            //ConChim.BackColor = Color.FromArgb(0, 135, 147);
+            // Set timer
+            Timer_Chay.Interval = 1;
+            //Timer_Chay.Start();
+            // Buton Play
+            
+            Play.Location = new Point(250,270);
+            Play.Size = new Size(120, 50);
+            Play.BackColor = Color.FromArgb(0, 135, 147);
+            TimerChim.Interval = 100;
+            // TimerChim.Start();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Refesh();
+        }
+        bool check;
+        int DiemSo = 0;
+        private void Timer_Chay_Tick(object sender, EventArgs e)
+        {
+            Play.Location = new Point(0, 0);
+            check = false;
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint | ControlStyles.DoubleBuffer, true);
+            x_cap1 -= 10;
+            x_cap2 -= 10;
+            x_cap3 -= 10;
+            OngCongTren1.Location = new Point(x_cap1, y_ongtren1);
+            y_ongduoi1 = (DoChenhLechGiua2Ong + (OngCongTren1.Height + y_ongtren1));
+            OngCongDuoi1.Location = new Point(x_cap1, y_ongduoi1);
+            // ống 2
+            OngCongTren2.Location = new Point(x_cap2, y_ongtren2);
+            y_ongduoi2 = (DoChenhLechGiua2Ong + (OngCongTren2.Height + y_ongtren2));
+            OngCongDuoi2.Location = new Point(x_cap2, y_ongduoi2);
+            // ống 3
+            OngCongTren3.Location = new Point(x_cap3, y_ongtren3);
+            y_ongduoi3 = (DoChenhLechGiua2Ong + (OngCongTren3.Height + y_ongtren3));
+            OngCongDuoi3.Location = new Point(x_cap3, y_ongduoi3);
+            if (x_cap1 + OngCongTren1.Width <= 0)
+            {
+                Random rd = new Random();
+                // random độ cao
+                y_ongtren1 = rd.Next(-550, -250);
+                x_cap1 = 850 + OngCongTren2.Width + 350; // mỗi ống cách nhau 250
+                DiemSo++;
+                OngCongTren1.Location = new Point(x_cap1, y_ongtren1);
+                OngCongDuoi1.Location = new Point(x_cap1, y_ongduoi1);
+                
+            }
+            if (x_cap2 + OngCongTren2.Width <= 0)
+            {
+                x_cap2 = 850 + OngCongDuoi1.Width + 350;
+                DiemSo++;
+                // Random y: 1000 + y = docao;
+                Random rd = new Random();
+                y_ongtren2 = rd.Next(-550, -250);
+                OngCongTren2.Location = new Point(x_cap2, y_ongtren2);
+                OngCongDuoi2.Location = new Point(x_cap2, y_ongduoi2);
+
+            }
+            if (x_cap3 + OngCongTren3.Width <= 0)
+            {
+                
+                x_cap3 = 850 + OngCongDuoi2.Width + 350;
+                DiemSo++;
+                // Random y: 1000 + y = docao;
+                Random rd = new Random();
+                y_ongtren3 = rd.Next(-550, -250);
+                OngCongTren3.Location = new Point(x_cap3, y_ongtren3);
+                OngCongDuoi3.Location = new Point(x_cap3, y_ongduoi3);
+            }
+            lblDiemSo.Text = "Điểm : "+ DiemSo.ToString();
+            //if (x_cap1 <= 200 || x_cap2 <= 200 || x_cap3 <= 200)
+            //{
+            //    check = true;
+            //    TimerChim3.Interval = 200;
+            //    TimerChim3.Start();
+            //}
+        }
+        int demClickPlay = 1;
+        private void Play_Click(object sender, EventArgs e)
+        {
+            if (demClickPlay % 2 != 0)
+            {
+                Timer_Chay.Start();
+                TimerChim.Start();
+                TimerChim.Interval = 100;
+            }
+            else
+            {
+                Timer_Chay.Stop();
+                TimerChim.Stop();
+            }
+            demClickPlay++;
+        }
+        int dem = 1;
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (dem % 2 != 0 )
+            {
+                SoundPlayer sound = new SoundPlayer("Fly.wav");
+                sound.LoadAsync();
+                sound.Play(); 
+            }
+            
+            TimerChim2.Stop();
+            //TimerChim3.Stop();
+            if (e.KeyCode == Keys.Space)
+            {
+                if (y_ConChim >= 0)
+                {
+                    y_ConChim -= 50;
+                    ConChim.Image = Image.FromFile(@"chimlenpts.png");
+                    ConChim.Size = new Size(111, 91);
+                }
+            }
+            dem++;
+        }
+
+        private void TimerChim_Tick(object sender, EventArgs e)
+        {
+            //this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint | ControlStyles.DoubleBuffer, true);
+            if (y_ConChim + ConChim.Height <= this.Height)
+            {
+                y_ConChim += 15;
+                this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint | ControlStyles.DoubleBuffer, true);
+                bool Check_Play = false;
+                if (x_ConChim + ConChim.Width >= x_cap1 && x_ConChim + ConChim.Width <= x_cap1 + OngCongTren1.Width)
+                {
+                    if (y_ConChim <= 709 + y_ongtren1 || y_ConChim + ConChim.Height >= y_ongduoi1)
+                    {
+                        Timer_Chay.Stop();
+                        //TimerChim.Stop();
+                        if (TimerChim.Interval == 100)
+                        {
+                            Check_Play = true;
+                            SoundPlayer sound = new SoundPlayer("Dead.wav");
+                            sound.LoadAsync();
+                            sound.Play(); 
+                            
+                        }
+                        TimerChim.Interval = 300;
+                        
+                    }
+                }
+                ConChim.Location = new Point(x_ConChim, y_ConChim);
+                if (x_ConChim + ConChim.Width >= x_cap2 && x_ConChim + ConChim.Width <= x_cap2 + OngCongTren1.Width)
+                {
+                    if (y_ConChim <= 709 + y_ongtren2 || y_ConChim + ConChim.Height >= y_ongduoi2)
+                    {
+                        Timer_Chay.Stop();
+                        if (TimerChim.Interval == 100)
+                        {
+                            Check_Play = true;
+                            SoundPlayer sound = new SoundPlayer("Dead.wav");
+                            sound.LoadAsync();
+                            sound.Play();
+                        }
+                        TimerChim.Interval = 300;
+                    }
+                }
+                ConChim.Location = new Point(x_ConChim, y_ConChim);
+                if (x_ConChim + ConChim.Width >= x_cap3 && x_ConChim + ConChim.Width <= x_cap3 + OngCongTren1.Width)
+                {
+                    if (y_ConChim <= 709 + y_ongtren3 || y_ConChim + ConChim.Height >= y_ongduoi3)
+                    {
+                        Timer_Chay.Stop();
+                        if (TimerChim.Interval == 100)
+                        {
+                            Check_Play = true;
+                            SoundPlayer sound = new SoundPlayer("Dead.wav");
+                            sound.LoadAsync();
+                            sound.Play();
+                        }
+                        TimerChim.Interval = 300;
+                    }
+                }
+                if (Check_Play == true)
+                {
+                    if (MessageBox.Show("Điểm: " + DiemSo.ToString() + "\n Ê có chơi tiếp không ?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        Timer_Chay.Stop();
+                        TimerChim.Stop();
+                        Refesh();
+                        DiemSo = 0;
+                    }
+                    else
+                    {
+                        Application.Exit();
+                    }
+                }
+                ConChim.Location = new Point(x_ConChim, y_ConChim);
+                
+            }
+            
+        }
+
+        private void TimerChim2_Tick(object sender, EventArgs e)
+        {
+            ConChim.Image = Image.FromFile(@"chimxuong2pts.png");
+            ConChim.Size = new Size(146, 136);
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            ConChim.Image = Image.FromFile(@"chim.png");
+            ConChim.Size = new Size(122, 80);
+            TimerChim2.Stop();
+            TimerChim2.Interval = 400;
+            TimerChim2.Start();
+        }
+        private void TimerChim3_Tick(object sender, EventArgs e)
+        {
+        }
+    }
+}
